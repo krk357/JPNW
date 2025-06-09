c439b43662e83bb2877ba2561a417ba92902197d
krk357 <ville.aleksi.virkkunen@gmail.com>
2025-06-09 09:24:52 +0300

    Add Generalized linear model

--- Japan Women.R
+++ Japan Women.R
@@ -61,3 +61,79 @@ jpn$preference <- recode(jpn$Q111, "1 = 'Protect the Economy'; 2 = 'Protect the 
 jpn$pref <- factor(jpn$preference, levels=c("Dont know", "Protect the Economy", "Protect the Nature"))
 
 table(jpn$pref)
+
+# Independent Variables
+
+# Age
+
+table(jpn$Q262)
+jpn$age <- recode(jpn$Q262, "-5=NA; -2=NA; -1=NA")
+table(jpn$age)
+
+# Gender
+
+table(jpn$Q260)
+jpn$gender <- recode(jpn$Q260, "1=0; 2=1; else=NA")
+table(jpn$gender)
+
+# Education
+
+table(jpn$Q275)
+jpn$educ <- recode(jpn$Q275, "-5=NA; -2=NA; -1=NA; 0=0; 1:3=1; 4:5=2; 6:7=3; 8=4")
+table(jpn$educ)
+
+# Income
+
+table(jpn$Q288)
+jpn$income <- recode(jpn$Q288, "-5=NA; -2=NA; -1=NA")
+table(jpn$income)
+
+# Interest of Politics
+
+table(jpn$Q199)
+jpn$polint <- recode(jpn$Q199, "-5=NA; -2=NA; -1=NA; 1:2=1; 3:4=0")
+table(jpn$polint)
+
+# Ideology
+
+table(jpn$Q240)
+jpn$ideology <- recode(jpn$Q240, "-5=NA; -4=NA; -2=NA; -1=NA")
+table(jpn$ideology)
+
+# rural vs suburban
+
+table(jpn$H_URBRURAL)
+jpn$urbrural <- recode(jpn$H_URBRURAL, "-5=NA; 2=0; 1=1")
+table(jpn$urbrural)
+
+# Economic satisfaction -> this? Prospective economic evaluations
+
+table(jpn$Q50)
+jpn$econsat <- recode(jpn$Q50, "-5=NA; -2=NA; -1=NA")
+table(jpn$econsat)
+
+####### Means and standard deviations #############
+
+mat1 <- data.frame(
+  dunno = jpn$dunnos,
+  natu = jpn$pronatu,
+  econ = jpn$proecon
+)
+cor(na.omit(mat1))
+
+descr(jpn$dunnos)
+descr(jpn$pronatu)
+descr(jpn$proecon)
+
+###################################################
+########## Generalized Linear Model ###############
+###################################################
+
+dunnos.mod <- glm(dunnos ~ age  + gender + educ + income + polint + ideology + urbrural + econsat, family = binomial, data = jpn)
+summary(dunnos.mod)
+
+pronatu.mod <- glm(pronatu ~ age + gender + educ + income + polint + ideology  + urbrural + econsat, family = binomial, data = jpn)
+summary(pronatu.mod)
+
+proecon.mod <- glm(proecon ~ age + gender + educ + income + polint + ideology  + urbrural + econsat, family = binomial, data = jpn)
+summary(proecon.mod)
